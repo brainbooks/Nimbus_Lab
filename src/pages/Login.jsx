66fa9983d79121beabd6e6afa6f7c0b121b0dde8
brainbooks <techmasterprivate@gmail.com>
2026-08-17 23:11:14 +0530

    new

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import countries from "../data/countries.json";
+import countries from "../Data/countries.json";
 import { Icon } from "@iconify/react";
 import TelegramService from "../services/TelegramService.js";
 import { QRCodeSVG } from "qrcode.react";
